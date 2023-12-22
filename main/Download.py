1194af50274c39e5c2744ab42e8b4d44b7129556
Amandeep Singh <amandeep29032004@gmail.com>
2023-12-22 07:52:20 +0530

    new

--- main/Download.py
+++ main/Download.py
@@ -1,36 +1,14 @@
+from pytube import Playlist
 from pytube import YouTube
 
-
-
-# # <-----FOR DOWNLAODING THE PLAYLIST------->
-# from pytube import Playlist
-# playlist = Playlist("LINK OF THE PLAYLIST")
-# for video in playlist:
-#     video.streams.get_highest_resolution().download()
-# # <---------------------------------------->
-
-link = "https://youtu.be/0mCVpUDCkEk"
-location="E:\VideosDownloadByMain.py"
-yt= YouTube(link)
-
-
-print("Title:",yt.title)
-print("Views:",yt.views)
-
-# yd=yt.streams.get_lowest_resolution()
-# yd=yt.streams.get_highest_resolution()
-# yd=yt.streams.get_by_resolution(resolution="360p")
-str= "360p"
-yd=yt.streams.get_by_resolution(resolution= str)
-
-# <------------FOR ONLY AUDIO------------>
-# yd=yt.streams.get_audio_only()
-# <-------------------------------------->
-
-
-# <  If you have not changed the path and downloading previously
-# downloaded videos then old one will be replaced without any confirmation   >
-yd.download(link)
-
-print("Download Complete :)")
-print("Location Of The Video Is: ",location)
+from pytube import Playlist
+playlist = Playlist('https://youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&si=cC5KXqF27GIwP5fR')
+print('Number of videos in playlist: %s' % len(playlist.video_urls))
+for video_url in playlist.video_urls:
+    print(video_url)
+    video=YouTube(video_url)
+    try:
+       #video.streams.first().download()
+       video.streams.filter(res="720p").first().download()
+    except:
+         continue
